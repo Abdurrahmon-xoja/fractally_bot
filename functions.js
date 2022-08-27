98d210e2689be1106049a45e044e242583b0b89b
Abdurrahmon-xoja <abdurrrahmont@icloud.com>
2022-08-27 17:52:14 +0500

    ah created bot

--- functions.js
+++ functions.js
@@ -1,42 +1,91 @@
-const fs = require('fs');
+const {GoogleSpreadsheet} = require('google-spreadsheet');
+const fs = require('fs')
+const doc = new GoogleSpreadsheet('1XDyeUDxGKOUDA9u4usTIkIiA4GO7GAudBosSnHaFA1U');
+const credentials = JSON.parse(fs.readFileSync("googleSheetsKeys.json"));
 
-let jsonData = fs.readFileSync('data.json');
-let data = JSON.parse(jsonData);
+async function register(hiveID, telegramID) {
+    await doc.useServiceAccountAuth({
+        client_email: credentials.client_email,
+        private_key: credentials.private_key,
+    });
 
-
-// result of --'s  meeting this hive account level is -- and avg
-function getHive(hiveID){
-    let result
-    data.forEach(info => {
-        if(hiveID == info.name){
-            let objectValue = Object.values(info.levels)
-            let revers = objectValue.reverse()
-            let avg = 0
-            for(let i = 0; i <= 5; i++){
-                avg += +revers[i]
-            }
-            objectValue.reverse()
-            result = `result of ${objectValue.length}'s  meeting ${hiveID} hive account level is ${objectValue[objectValue.length - 1]} and avg is ${Math.round(avg / 6)}`
+    await doc.loadInfo(); // loads document properties and worksheets
+    const sheet = await doc.sheetsByIndex[1]; // first sheet
+    const rows = await sheet.getRows();
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].hive == hiveID) {
+            rows[i].telegramIDs = telegramID
+            await rows[i].save();
         }
-    })
+    }
+}
+
+
+async function getHive(telegramID) {
+    await doc.useServiceAccountAuth({
+        client_email: credentials.client_email,
+        private_key: credentials.private_key,
+    });
+
+    await doc.loadInfo(); // loads document properties and worksheets
+    const sheet = await doc.sheetsByIndex[1]; // first sheet
+    const rows = await sheet.getRows();
+
+    let result
+
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].telegramIDs == telegramID) {
+            result = rows[i].hive
+        }
+    }
+
     return result
 }
 
+async function getLevel(telegramID) {
 
+    await doc.useServiceAccountAuth({
+        client_email: credentials.client_email,
+        private_key: credentials.private_key,
+    });
 
-function isExist(hiveID){
-    let exist = false
-    data.forEach(info => {
-        if(hiveID == info.name){
-            exist = true
+    await doc.loadInfo(); // loads document properties and worksheets
+    const sheet = await doc.sheetsByIndex[0]; // first sheet
+    const userHive = await getHive(telegramID)
+    const rows = await sheet.getRows();
+
+    let arrayInfo
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].hive == userHive) {
+            let some = await sheet.getRows({limit: 1, offset: i});
+            arrayInfo = some[0]._rawData
         }
-    })
+    }
 
-    return exist
+    // working with info
+    let lastLevel = arrayInfo[arrayInfo.length - 1]
+    let totalLevel = 0
+    for (let i = 1; i < arrayInfo.length; i++) {
+        totalLevel += +arrayInfo[i]
+    }
+    let allMeeting = 0
+    for (let i = 1; i < arrayInfo.length; i++)
+        if (+arrayInfo[i] !== 0) {
+            allMeeting += 1
+        }
+
+    let lastMeetingDate = await sheet.getRows({limit: 1, offset: -1});
+    let some = lastMeetingDate[0]._rawData
+    let some2 = some[some.length -1]
+    console.log(some2)
+    return`Last meeting #: ${some2}\nLast meeting level: ${lastLevel}\nTotal meetings participated: ${allMeeting}\nTotal respect earned: ${totalLevel}\n`
+    // console.log(`Last meeting you level was <b>${lastLevel}</b> you participated in <b>${allMeeting}</b> meetings and your total respect is <b>${totalLevel}</b>`)
 }
+
+// getLevel('215197299')
 
 
 module.exports = {
-    getHive,
-    isExist
+    register,
+    getLevel
 }
--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-const {Telegraf} = require('telegraf')
+const {Telegraf, Markup, Composer, Scenes, session} = require('telegraf')
 require('dotenv').config()
 
-const {getHive, isExist} = require('./functions')
+const {register, getLevel} = require('./functions')
 
 const bot = new Telegraf(process.env.telegramBotToken)
 
 
-bot.start(ctx => {
-    ctx.reply('write hive account')
+const startRegistration = new Composer()
+startRegistration.on('message', async ctx => {
+    await ctx.replyWithSticker('CAACAgIAAxkBAAICg2MJqogB2PSzGmrweALXNt7RYd7yAAIBAQACVp29CiK-nw64wuY0KQQ')
+    await ctx.reply(`Hello 👋 ƒractally member ${ctx.from.first_name}`, )
+    await ctx.reply('I am ƒractally_level bot 😊, I show your information about last ƒractally meeting and notification about your level.Because you here first time can you write you hive username please ❤️',
+        Markup
+            .keyboard([
+                ['Show my level'],
+            ])
+            .oneTime()
+            .resize())
+
+    return ctx.wizard.next()
 })
 
 
-bot.on('message', ctx => {
-    if (isExist(ctx.message.text)) {
-        ctx.reply(getHive(ctx.message.text))
-        ctx.reply('write hive account')
-    } else {
-        ctx.reply('this hive account not exist try another one')
-    }
+const endRegistration = new Composer()
+endRegistration.on('message', async ctx => {
+    register(ctx.message.text, ctx.from.id)
 
-
+    await ctx.reply(await getLevel(ctx.from.id),
+        Markup.
+        inlineKeyboard([
+            Markup.button.url('Click here for Google sheets' , "http://telegraf.js.org"),
+        ]))
+    return ctx.scene.leave()
 })
 
+
+bot.hears('Show my level', async ctx => {
+    await ctx.reply(await getLevel(ctx.from.id),
+        Markup.
+        inlineKeyboard([
+        Markup.button.url('Click here for Google sheets' , "https://docs.google.com/spreadsheets/d/1XDyeUDxGKOUDA9u4usTIkIiA4GO7GAudBosSnHaFA1U/edit#gid=0"),
+    ]))
+})
 
 bot.launch()
+
+
+const Registration = new Scenes.WizardScene('sceneWizardRegistration', startRegistration, endRegistration)
+const stage = new Scenes.Stage([Registration])
+bot.use(session())
+bot.use(stage.middleware())
+
+
+bot.start(async ctx => {
+    console.log('it is working')
+    ctx.scene.enter('sceneWizardRegistration')
+})
