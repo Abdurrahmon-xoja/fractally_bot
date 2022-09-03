027dfd9739fe650fb7ae2b9472f16288843feeeb
Abdurrahmon-xoja <abdurrrahmont@icloud.com>
2022-09-03 20:39:25 +0500

    did a lot of staff

--- functions.js
+++ functions.js
@@ -103,10 +103,19 @@ async function getLevelFront(telegramID) {
     let lastMeetingDate = await sheet.getRows({limit: 1, offset: -1});
     let some = lastMeetingDate[0]._rawData
     let some2 = some[some.length - 1]
-    console.log(some2)
-    return `Last meeting #: ${some2}\nLast meeting level: ${lastLevel}\nTotal meetings participated: ${allMeeting}\nTotal respect earned: ${totalLevel}\n`
+
+    // console.log(arrayInfo)
+    let userArray = arrayInfo.slice(1)
+        let avg = 0
+        userArray.reverse()
+        for(let i = 0; i < 6; i++ ){
+            avg += +userArray[i]
+        }
+    return `Last meeting #: ${some2}\nLast meeting level: ${lastLevel}\nTotal meetings participated: ${allMeeting}\nTotal respect earned: ${totalLevel}\nAverage result: ${Math.ceil(avg / 6)}`
     // console.log(`Last meeting you level was <b>${lastLevel}</b> you participated in <b>${allMeeting}</b> meetings and your total respect is <b>${totalLevel}</b>`)
 }
+
+getLevelFront('215197299')
 
 async function getLevelBack(hiveID) {
     await doc.useServiceAccountAuth({
@@ -154,11 +163,133 @@ async function telegramUsers() {
 }
 
 
+async function isExistInDb(hiveID) {
+    await doc.useServiceAccountAuth({
+        client_email: credentials.client_email,
+        private_key: credentials.private_key,
+    });
+
+    await doc.loadInfo(); // loads document properties and worksheets
+    const sheet = await doc.sheetsByIndex[0]; // first sheet
+    const rows = await sheet.getRows();
+
+    // console.log(rows[0].hive)
+
+    let exist = false
+
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].hive == hiveID) {
+            exist = true
+        }
+    }
+
+    console.log(exist)
+    return exist
+}
+
+async function showOthers(hiveID) {
+    await doc.useServiceAccountAuth({
+        client_email: credentials.client_email,
+        private_key: credentials.private_key,
+    });
+
+    await doc.loadInfo(); // loads document properties and worksheets
+    const sheet = await doc.sheetsByIndex[0]; // first sheet
+    // const userHive = await getHive(telegramID)
+    const rows = await sheet.getRows();
+
+    let arrayInfo
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i].hive == hiveID) {
+            let some = await sheet.getRows({limit: 1, offset: i});
+            arrayInfo = some[0]._rawData
+        }
+    }
+
+    // working with info
+    let lastLevel = arrayInfo[arrayInfo.length - 1]
+    let totalLevel = 0
+    for (let i = 1; i < arrayInfo.length; i++) {
+        totalLevel += +arrayInfo[i]
+    }
+    let allMeeting = 0
+    for (let i = 1; i < arrayInfo.length; i++)
+        if (+arrayInfo[i] !== 0) {
+            allMeeting += 1
+        }
+
+    let lastMeetingDate = await sheet.getRows({limit: 1, offset: -1});
+    let some = lastMeetingDate[0]._rawData
+    let some2 = some[some.length - 1]
+
+    let userArray = arrayInfo.slice(1)
+    let avg = 0
+    userArray.reverse()
+    for(let i = 0; i < 6; i++ ){
+        avg += +userArray[i]
+    }
+    return `Last meeting #: ${some2}\nLast meeting level: ${lastLevel}\nTotal meetings participated: ${allMeeting}\nTotal respect earned: ${totalLevel}\nAverage result: ${Math.ceil(avg / 6)}`
+    // console.log(`Last meeting you level was <b>${lastLevel}</b> you participated in <b>${allMeeting}</b>
+    // return `Last meeting #: ${some2}\nLast meeting level: ${lastLevel}\nTotal meetings participated: ${allMeeting}\nTotal respect earned: ${totalLevel}\n`
+
+}
+
+async function top12(){
+    // await doc.useServiceAccountAuth({
+    //     client_email: credentials.client_email,
+    //     private_key: credentials.private_key,
+    // });
+    //
+    // await doc.loadInfo(); // loads document properties and worksheets
+    // const sheet = await doc.sheetsByIndex[0]; // first sheet
+    // const rows = await sheet.getRows();
+
+    // console.log(rows)
+    // for(let i = 105; i < rows.length; i++){
+    //     let row = await sheet.getRows({limit: 1, offset: i})
+    //     let userName = row[0]._rawData[0]
+    //     let userArray = row[0]._rawData.slice(1)
+    //     let avg = 0
+    //     userArray.reverse()
+    //     for(let i = 0; i < 6; i++ ){
+    //         avg += +userArray[i]
+    //     }
+    //     avgObject[userName] = Math.ceil(avg / 6)
+    //     console.log(avgObject)
+    // }
+
+    let jsonAvgData = fs.readFileSync('avg.json');
+    let dataAvg = JSON.parse(jsonAvgData);
+
+    const sortable = Object.fromEntries(
+        Object.entries(dataAvg).sort(([,a],[,b]) => a-b)
+    );
+
+    const reversedKeys = Object.keys(sortable).reverse();
+    const reversedValus = Object.values(sortable).reverse()
+
+    let result = ''
+    for(let i = 0; i < 12; i++){
+        result += `${i +1}.${reversedKeys[i]}: ${reversedValus[i]}\n`
+    }
+
+    let jsonData = fs.readFileSync('data.json');
+    let data = JSON.parse(jsonData);
+    let LastMeetingDb = data[0].date
+
+    // console.log(`Top 12 in ${LastMeetingDb}\n${result}`)
+    return `Average Top 12 for ${LastMeetingDb} \n${result}`
+}
+
+
 
 module.exports = {
     register,
     getLevelFront,
     getTelegramID,
     telegramUsers,
-    getLevelBack
+    getLevelBack,
+    isExistInDb,
+    showOthers,
+    top12
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {Telegraf, Markup, Composer, Scenes, session} = require('telegraf')
 require('dotenv').config()
 
-const {register, getLevelFront} = require('./functions')
+const {register, getLevelFront, isExistInDb , showOthers , top12} = require('./functions')
 
 const bot = new Telegraf(process.env.telegramBotToken)
 
@@ -9,47 +9,138 @@ const bot = new Telegraf(process.env.telegramBotToken)
 const startRegistration = new Composer()
 startRegistration.on('message', async ctx => {
     await ctx.replyWithSticker('CAACAgIAAxkBAAICg2MJqogB2PSzGmrweALXNt7RYd7yAAIBAQACVp29CiK-nw64wuY0KQQ')
-    await ctx.reply(`Hello 👋 ƒractally member ${ctx.from.first_name}`, )
-    await ctx.reply('I am ƒractally_level bot 😊, I show your information about hive account and notify  you.Because you here first time can you write you hive username please ❤️')
+    await ctx.reply(`Hello 👋 ${ctx.from.first_name}`)
+    await ctx.replyWithHTML(`Do you have hive account`, Markup
+        .keyboard([
+            ['Yes', 'No']
+        ])
+        .oneTime()
+        .resize()
+    )
+
+    // await ctx.reply('I am ƒractally_level bot 😊, I show your information about hive account and notify  you.Because you here first time can you write you hive username please ❤️')
+    // render buttons yes no
+    ctx.wizard.next()
 })
 
+
+const sortingRegistration = new Composer()
+sortingRegistration.on('message', async ctx => {
+    if (ctx.message.text == 'No') {
+        await ctx.replyWithHTML('You can register <a href="https://hive.blog">here</a>')
+        return ctx.scene.leave()
+    } else if (ctx.message.text == 'Yes') {
+        await ctx.reply('Please write you hive account')
+        ctx.wizard.next()
+    }
+})
 
 const endRegistration = new Composer()
 endRegistration.on('message', async ctx => {
-    register(ctx.message.text, ctx.from.id)
+    let checked = false
+    if (await isExistInDb(ctx.message.text)) {
+        register(ctx.message.text, ctx.from.id)
+        await ctx.reply('You have been registered' ,
+            Markup
+                .keyboard([
+                    ['Show my level'],
+                    ['Show others hive'],
+                    ['Top12']
+                ])
+                .oneTime()
+                .resize()
+        )
+        checked = true
+    } else {
+        await ctx.reply(`we do not have account ${ctx.message.text} try again` ,)
+    }
 
-    await ctx.reply(await getLevelFront(ctx.from.id),
-        Markup
-            .keyboard([
-                ['Show my level'],
-            ])
-            .oneTime()
-            .resize())
+    if(checked){
+        return ctx.scene.leave()
+    }
+})
+// end scene
+
+
+const startStats = new Composer()
+startStats.on('message' , async ctx => {
+    await ctx.reply('Write account stats you want see')
+    ctx.wizard.next()
+})
+
+const endStats = new Composer()
+endStats.on('message' , async ctx=>{
+    if(isExistInDb(ctx.message.tex)){
+        await ctx.reply(await showOthers(ctx.message.text) ,
+            Markup
+                .keyboard([
+                    ['Show my level'],
+                    ['Show others hive'],
+                    ['Top12']
+                ])
+                .oneTime()
+                .resize()
+        )
+    }else {
+        await ctx.reply(`${ctx.message.text} not exist `,
+            Markup
+                .keyboard([
+                    ['Show my level'],
+                    ['Show others hive'],
+                    ['Top12']
+                ])
+                .oneTime()
+                .resize()
+        )
+    }
     return ctx.scene.leave()
 })
-// end sene
+
 
 bot.hears('Show my level', async ctx => {
     await ctx.reply(await getLevelFront(ctx.from.id),
         Markup
             .keyboard([
                 ['Show my level'],
+                ['Show others hive'],
+                ['Top12']
             ])
             .oneTime()
             .resize()
-        )
+    )
 })
+
+bot.hears('Top12', async ctx => {
+    ctx.reply(await top12(), Markup
+        .keyboard([
+            ['Show my level'],
+            ['Show others hive'],
+            ['Top12']
+        ])
+        .oneTime()
+        .resize()
+    )
+})
+
+
+
+
 
 bot.launch()
 
 
-const Registration = new Scenes.WizardScene('sceneWizardRegistration', startRegistration, endRegistration)
-const stage = new Scenes.Stage([Registration])
+const Registration = new Scenes.WizardScene('sceneWizardRegistration', startRegistration, sortingRegistration, endRegistration)
+const showStats = new Scenes.WizardScene('sceneWizardStats', startStats, endStats)
+const stage = new Scenes.Stage([Registration,showStats])
 bot.use(session())
 bot.use(stage.middleware())
 
 
 bot.start(async ctx => {
-    console.log('it is working')
     ctx.scene.enter('sceneWizardRegistration')
+})
+
+
+bot.hears('Show others hive' , async ctx =>{
+    ctx.scene.enter('sceneWizardStats')
 })
